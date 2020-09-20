1d5d1dce205c012158e27380ef5cd1c658956203
ryoma116 <uma19880116>
2020-09-21 05:08:10 +0900

    create hello.html view

--- helloworldproject/helloworldproject/urls.py
+++ helloworldproject/helloworldproject/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import hello_world_function
+from .views import hello_world_function, HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworld/', hello_world_function)
+    path('helloworld/', hello_world_function),
+    path('helloworld2/', HelloWorldView.as_view())
 ]
--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 def hello_world_function(request):
     return HttpResponse('<h1>hello world</h1>')
+
+
+class HelloWorldView(TemplateView):
+    template_name = 'hello.html'
